c9e073290c0a50ca4e682806367f679ec68013de
Logan <logansailer0633@gamil.com>
2024-10-16 01:56:04 -0400

    completed form

--- chacha/src/components/Hero.jsx
+++ chacha/src/components/Hero.jsx
@@ -1,10 +1,31 @@
 export default function Hero() {
-    return (
-        <>
-        <h1>Coffee Tracking for Coffee <abbr title="An enthusiast or devotee">Fiends</abbr></h1>
-        <div className="benefits-list">
-            <h3 className="">Try <span className="text-gradient">Caffiend</span></h3>
+  return (
+    <>
+      <h1>
+        Coffee Tracking for Coffee{" "}
+        <abbr title="An enthusiast or devotee">Fiends</abbr>
+      </h1>
+      <div className="benefits-list">
+        <h3 className="font-bolder">
+          Try <span className="text-gradient">Caffiend</span> and start...
+        </h3>
+        <p>✔️ Tracking every coffee</p>
+        <p>✔️ Measuring blood caffeine levels</p>
+        <p>✔️ Quantifying your addiction</p>
+      </div>
+      <div className="card info-card">
+        <div>
+          <i className="fa-solid fa-circle-info"></i>
+          <h3>Did you know...</h3>
         </div>
-        </>
-    )
+        <h5>That caffeine&apos;s half life is about 5 hours?</h5>
+        <p>
+          This means that after 5 hours, 1/2 tje caffeine you consumed is still
+          in your system, keeping you alert longer! So if you drink a cup of
+          coffee with 200mg of caffeine, 5 hours later you&apos;ll still have
+          100mg of caffiene in your system.
+        </p>
+      </div>
+    </>
+  );
 }
--- chacha/src/components/Layout.jsx
+++ chacha/src/components/Layout.jsx
@@ -1,6 +1,26 @@
 export default function Layout({ children }) {
-  const header = <header></header>;
-  const footer = <footer></footer>;
+  const header = (
+    <header>
+      <div>
+        <h1 className="text-gradient">Chacha</h1>
+        <p>For Tea Insatiates</p>
+      </div>
+      <button>
+        <p>Sign up free</p>
+        <i className="fa-solid fa-mug-hot"></i>
+      </button>
+    </header>
+  );
+  const footer = (
+    <footer>
+      <p>
+        <span className="text-gradient">Chacha</span> was made by{" "}
+        <a href="https://logan-sailer.netlify.app/" target="_blank">
+          Logan Sailer
+        </a>
+      </p>
+    </footer>
+  );
   return (
     <>
       {header}
